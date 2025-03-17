5ecae168f04e5e3195baac1c7a8150dc3381836c
Thomas Järvstrand <tjarvstrand@gmail.com>
2025-03-17 10:21:31 +0100

    Add animated_fab

---
+++ packages/animated_fab/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Some items', style: Theme.of(context).textTheme.titleMedium), centerTitle: true),
+      body: ListView(children: [for (final i in Iterable<int>.generate(50)) ListTile(title: Text('Item $i'))]),
+    );
+  }
+}
